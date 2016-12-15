a08ba3129431350b886e70b465838bc74ebe7080
Jhefri1999 <jhefri.melendez@speeracademy.org>
2016-12-15 12:12:01 -0600

    Almost Done
    Only need to make my links better

--- script.js
+++ script.js
@@ -3,10 +3,33 @@ $(document).ready(function(){
         $("img").fadeTo("slow", 1);
     });
     $("img").mouseleave(function(){
-        $("img").fadeTo("fast", 0.5);
+        $("img").fadeTo("slow", 0.5);
     });
     $('#button').click(function(){
         var toAdd = $('input[name=checkListItem]').val();
     })
 })    
+$(document).ready(function() {
+    $('input').focus(function(){
+        $(this).css('outline-color','#FF0000')
+    });
+});
+$(document).ready(function() {
+    $(document).keydown(function(key) {
+        switch(parseInt(key.which,10)) {
 
+        case 37:
+    $('img').animate({left: "-=10px"},'fast');
+        break;
+        case 38:
+    $('img').animate({top: "-=10px"},'fast');
+        break;
+        case 39:
+    $('img').animate({left: "+=10px"},'fast');
+        break;
+        case 40:
+    $('img').animate({top: "+=10px"},'fast');
+        break;
+        };
+    });
+});
